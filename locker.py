d9b9ee6011eb31ee1b2b9990c0d5104a5e09ac97
sarahsindet <sarahsindet@gmail.com>
2020-06-29 22:01:10 +0300

    used init method to determine the crdits to be stored

--- locker.py
+++ locker.py
@@ -33,3 +33,10 @@ class Credentials():
     Create credentials class to create new objects of credentials
     '''
     credentials_list = []
+def __init__(self, account, userName, password):
+        '''
+        method that defines user credentials to be stored
+        '''
+        self.account = account
+        self.userName = userName
+        self.password = password
