c71bcd730843ca078e055b99e71b92b0c3ce9ca9
sarahsindet <sarahsindet@gmail.com>
2020-06-29 23:01:10 +0300

    teared down the method after run

--- locker_test.py
+++ locker_test.py
@@ -19,4 +19,29 @@ def test_init(self):
         '''
         self.assertEqual(self.new_user.username,"SarahSindet")
         self.assertEqual(self.new_user.password,"210sda38")
+  def test_save_user(self):
+        '''
+        test case to test if a new user object has been saved into the User list
+        '''
+        
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the credentials class behaviours.
+    '''
+
+    def setUp(self):
+        '''
+        Method that runs before each credentials test case.
+        '''
+        self.new_credential = Credentials("Gmail","dorydory","143811jsa")
+
+
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list = []
