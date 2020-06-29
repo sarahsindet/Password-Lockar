48d55798a96e433f920f4511c8ef93d772b6f721
sarahsindet <sarahsindet@gmail.com>
2020-06-29 23:22:33 +0300

    defined function to save users and display them

--- password.py
+++ password.py
@@ -10,3 +10,15 @@ def create_new_user(username, password):
     '''
     new_user = User(username, password)
     return new_user
+def save_user(user):
+    '''
+    Function to save a new user
+    '''
+    user.save_user()
+
+
+def display_user():
+    """
+    Function to display existing user
+    """
+    return User.display_user()
