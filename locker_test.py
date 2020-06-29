b1b5ba4d717c54139524b35d7a0e10d825c7c37f
sarahsindet <sarahsindet@gmail.com>
2020-06-29 22:57:48 +0300

    set up the init method

--- locker_test.py
+++ locker_test.py
@@ -13,3 +13,10 @@ def setUp(self):
         Method to run before each user test cases.
         '''
         self.new_user = User("FeistyDory","210sda38")
+def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.username,"FeistyDory")
+        self.assertEqual(self.new_user.password,"210sda38")
+
