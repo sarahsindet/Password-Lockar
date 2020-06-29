ebd515b262fe573d15ccbba7cb3db606dd6dbfa6
sarahsindet <sarahsindet@gmail.com>
2020-06-29 22:50:04 +0300

    created atest on user credits

--- locker_test.py
+++ locker_test.py
@@ -1,3 +1,10 @@
 import unittest 
 from locker import User
 from locker import Credentials
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
