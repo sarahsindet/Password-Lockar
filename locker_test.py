922a03611c01e25df1d2e949cf87fa1109b2d7c7
sarahsindet <sarahsindet@gmail.com>
2020-06-29 23:03:49 +0300

    set up all the initia;isation credits

--- locker_test.py
+++ locker_test.py
@@ -45,3 +45,64 @@ class TestCredentials(unittest.TestCase):
         method that does clean up after each test case has run.
         '''
         Credentials.credentials_list = []
+ def test_init(self):
+        '''
+        Test case to check if a new Credentials instance has been initialized correctly
+        '''
+        self.assertEqual(self.new_credential.account,"Gmail")
+        self.assertEqual(self.new_credential.userName,"dorydory")
+        self.assertEqual(self.new_credential.password,"143811jsa") 
+
+
+    def save_credential_test(self):
+        '''
+        test case to test if the credential object is saved into the credentials list.
+        '''
+        self.new_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+
+    def test_save_many_accounts(self):
+        '''
+        test to check if we can save multiple credentials objects to our credentials list
+        '''
+        self.new_credential.save_details()
+        test_credential = Credentials("instagram","feistydory","qwerty") 
+        test_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+
+    def test_delete_credential(self):
+        '''
+        test method to test if we can remove an account credentials from our credentials_list
+        '''
+        self.new_credential.save_details()
+        test_credential = Credentials("instagram","feistydory","qwerty") 
+        test_credential.save_details()
+
+        self.new_credential.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+
+    def test_find_credential(self):
+        '''
+        test to check if we can find a credential entry by account name and display the details of the credential
+        '''
+        self.new_credential.save_details()
+        test_credential = Credentials("instagram","feistydory","qwerty") 
+        test_credential.save_details()
+
+        get_credential = Credentials.find_credential("instagram")
+
+        self.assertEqual(get_credential.account,test_credential.account)
+
+
+    def test_credential_exist(self):
+        '''
+        test to check if we can return a boolean based on whether we can or can't find the credential.
+        '''
+        self.new_credential.save_details()
+        get_credential = Credentials("instagram","feistydory","qwerty") 
+        get_credential.save_details()
+        credential_is_found = Credentials.if_credential_exist("instagram")
+        self.assertTrue(credential_is_found)
